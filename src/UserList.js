431ba0eec5c251a5f66fd5a4cbfaa86d2a26e187
HeeYoung-Youn <yhy1@naver.com>
2021-11-19 17:39:02 +0900

    Add: 배열에 항목 수정하기
    항목 수정 시 map을 사용해서 불변성을 유지하면서 항목 수정 가능

--- src/UserList.js
+++ src/UserList.js
@@ -1,21 +1,35 @@
 import React from "react";
 
-const User = ({ user, onRemove }) => {
-  const { username, email, id } = user;
+const User = ({ user, onRemove, onToggle }) => {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{username}</b>
+      <b
+        style={{
+          color: active ? "green" : "black",
+          cursor: "pointer",
+        }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
+      </b>
+      &nbsp;
       <span>({email})</span>
       <button onClick={() => onRemove(id)}>삭제</button>
     </div>
   );
 };
 
-const UserList = ({ users, onRemove }) => {
+const UserList = ({ users, onRemove, onToggle }) => {
   return (
     <div>
       {users.map((user) => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
