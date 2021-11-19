51008f2f030234e3f410b94ae87f98920fc124c7
HeeYoung-Youn <yhy1@naver.com>
2021-11-19 18:08:34 +0900

    Add: useMemo 사용하여 연산한 값 재사용하기
    useMemo를 사용해서 값에 변화가 있을 때에만 특정 동작을 하게 최적화 할 수 있음

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useMemo } from "react";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
+
+const countActiveUsers = (users) => {
+  console.log("활성 사용자 수를 세는중...");
+  return users.filter((user) => user.active).length;
+};
 
 function App() {
   const [inputs, setInputs] = useState({ username: "", email: "" });
@@ -63,6 +68,8 @@ function App() {
     );
   };
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
     <>
       <CreateUser
@@ -72,6 +79,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
