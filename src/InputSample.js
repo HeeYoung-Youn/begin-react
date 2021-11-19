7eea82d56c8a51c2b441789e7cd48acc6992c4fe
HeeYoung-Youn <yhy1@naver.com>
2021-11-19 16:16:37 +0900

    Add: useRef로 특정 DOM 선택하기
    useRef를 통해 특정 DOM에 직접 접근 가능

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const InputSample = () => {
   const [inputs, setInputs] = useState({
@@ -6,24 +6,29 @@ const InputSample = () => {
     nickname: "",
   });
 
+  const nameInput = useRef();
   const { name, nickname } = inputs;
 
   const onChange = (e) => {
     const { name, value } = e.target;
     console.log(inputs);
-    // 불변성을 지켜야만 입력 시 랜더링되므로
-    // 스프레드 문법을 사용해 기존의 값을 가져온 후
-    // 변화가 있는 부분만 덮어쓰기 해야 함
     setInputs({ ...inputs, [name]: value });
   };
 
   const onReset = () => {
     setInputs({ name: "", nickname: "" });
+    nameInput.current.focus();
   };
 
   return (
     <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
+      <input
+        name="name"
+        placeholder="이름"
+        onChange={onChange}
+        value={name}
+        ref={nameInput}
+      />
       <input
         name="nickname"
         placeholder="닉네임"
