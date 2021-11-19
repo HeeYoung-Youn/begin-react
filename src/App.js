fd242233ebba8547be29fbb1cbc89563ca05de2a
HeeYoung-Youn <yhy1@naver.com>
2021-11-19 14:41:57 +0900

    Add: JSX
    지켜야 할 JSX 규칙

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import Hello from "./Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
+
   return (
     <>
-      <Hello />
-      <div>안녕히계세요.</div>
+      {/* 주석 예시 */}
+      <Hello
+      // 주석 예시
+      />
+      <div style={style}>{name}</div>
+      <div className="gray-box" />
     </>
   );
 }
