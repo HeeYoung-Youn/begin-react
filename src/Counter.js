c1bc8c32f60cbfb191e34c0a72a2fb2565419308
HeeYoung-Youn <yhy1@naver.com>
2021-11-22 14:46:13 +0900

    Add: useReducer 예시
    useReducer를 사용해서 컴포넌트 외부에서 상태값을 관리할 수 있음

--- src/Counter.js
+++ src/Counter.js
@@ -1,10 +1,20 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      throw new Error("Unhandled action");
+  }
+};
 
 const Counter = () => {
-  const [number, setNumber] = useState(0);
-
-  const onIncrease = () => setNumber((prevNumber) => prevNumber + 1);
-  const onDecrease = () => setNumber(number - 1);
+  const [number, dispatch] = useReducer(reducer, 0);
+  const onIncrease = () => dispatch({ type: "INCREMENT" });
+  const onDecrease = () => dispatch({ type: "DECREMENT" });
 
   return (
     <div>
