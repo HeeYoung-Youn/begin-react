c82f81a036a8ff3054beaffffdbfc64ee65e8e50
HeeYoung-Youn <yhy1@naver.com>
2021-11-23 10:38:58 +0900

    Refactor: UserDispatch Context
    props를 통해 함수 전달 => UserDispatch Context를 통해 한번에 전달

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React, { useRef, useReducer, useMemo, useCallback } from "react";
+import React, { useReducer, useMemo, createContext } from "react";
 import CreateUser from "./CreateUser";
-import useInputs from "./useInputs";
 import UserList from "./UserList";
 
-const countActiveUsers = (users) => {
-  return users.filter((user) => user.active).length;
-};
+const countActiveUsers = (users) => users.filter((user) => user.active).length;
 
 const initialState = {
   users: [
@@ -54,46 +51,19 @@ const reducer = (state, action) => {
   }
 };
 
+export const UserDispatch = createContext(null);
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [form, onChange, reset] = useInputs({
-    username: "",
-    email: "",
-  });
-  const { username, email } = form;
-  const nextId = useRef(4);
   const { users } = state;
-
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: "CREATE_USER",
-      user: { id: nextId.current, username, email },
-    });
-    nextId.current += 1;
-    reset();
-  }, [email, username, reset]);
-
-  const onToggle = useCallback((id) => {
-    dispatch({ type: "TOGGLE_USER", id });
-  }, []);
-
-  const onRemove = useCallback((id) => {
-    dispatch({ type: "REMOVE_USER", id });
-  }, []);
-
   const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
-    <>
-      <CreateUser
-        username={username}
-        email={email}
-        onChange={onChange}
-        onCreate={onCreate}
-      />
-      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
       <div>활성 사용자 수 : {count}</div>
-    </>
+    </UserDispatch.Provider>
   );
 }
 
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
+import { UserDispatch } from "./App";
+import useInputs from "./useInputs";
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
+const CreateUser = () => {
+  const dispatch = useContext(UserDispatch);
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const nextId = useRef(4);
+
   return (
     <div>
       <input
@@ -15,7 +24,18 @@ const CreateUser = ({ username, email, onChange, onCreate }) => {
         onChange={onChange}
         value={email}
       />
-      <button onClick={onCreate}>등록</button>
+      <button
+        onClick={() => {
+          dispatch({
+            type: "CREATE_USER",
+            user: { id: nextId.current, username, email },
+          });
+          nextId.current += 1;
+          reset();
+        }}
+      >
+        등록
+      </button>
     </div>
   );
 };
