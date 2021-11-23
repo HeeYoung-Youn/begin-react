e3d05abb0632e8f282746ec65ffa547472e329e8
HeeYoung-Youn <yhy1@naver.com>
2021-11-23 11:28:37 +0900

    Refactor: reducer immer 적용
    immer를 통해 불변성을 지키면서 객체 혹은 배열을 더 손쉽게 업데이트 할 수 있음

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useReducer, useMemo, createContext } from "react";
+import produce from "immer";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
@@ -31,16 +32,13 @@ const reducer = (state, action) => {
   switch (action.type) {
     case "CREATE_USER":
       return {
-        inputs: initialState.inputs,
         users: state.users.concat(action.user),
       };
     case "TOGGLE_USER":
-      return {
-        ...state,
-        users: state.users.map((user) =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        ),
-      };
+      return produce(state, (draft) => {
+        const user = draft.users.find((user) => user.id === action.id);
+        user.active = !user.active;
+      });
     case "REMOVE_USER":
       return {
         ...state,
