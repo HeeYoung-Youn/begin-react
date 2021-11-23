979e2fc5fbc039fce4c9a166785dabe51051989a
HeeYoung-Youn <yhy1@naver.com>
2021-11-23 15:23:43 +0900

    Add: Sentry 연동
    componentDidCatch 시 에러내용 Sentry로 전송하게끔 연동

--- src/ErrorCatchApp.js
+++ src/ErrorCatchApp.js
@@ -2,11 +2,6 @@ import ErrorBoundary from "./ErrorBoundary";
 import User from "./User";
 
 const ErrorCatchApp = () => {
-  const user = {
-    id: 1,
-    usernmae: "heeyoung",
-  };
-
   return (
     <ErrorBoundary>
       <User />
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ErrorCatchApp from "./ErrorCatchApp";
+
+Sentry.init({
+  dsn: "https://8bc9b18a69e4492494aa1941949fb149@o1075758.ingest.sentry.io/6076635",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ErrorCatchApp />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
